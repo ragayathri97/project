24bce90581b2cb56ec9f08ce296a07b1fce6f5cd
ragayathri97 <ragayathri97@gmail.com>
2024-12-24 12:25:41 +0530

    todosir

---
+++ script/index.js
@@ -0,0 +1,4 @@
+import { navbar } from "./navbar.js";
+
+document.getElementById("nav").innerHTML = navbar()
+ 
--- script/navbar.js
+++ script/navbar.js
@@ -1,3 +1,14 @@
 const navbar = ()=>{
-     
-}
+     let card = ` <div id="nav-container>
+        <a id="logo" href="">My Personal App</a>
+    </div>
+    <div id= "nav-links>
+        <a href="signup.html">Signup</a>
+        <a href="login.html">Login</a>
+        <a href="todods.html">Todos</a>
+        <a href="expenses.html">Expenses</a>
+    </div> `;
+return card;
+    
+};
+export {navbar}
