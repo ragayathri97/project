69e5734914fdbe5807b20f6304dc5c5284cb6544
ragayathri97 <ragayathri97@gmail.com>
2024-12-24 22:21:46 +0530

    commit2

---
+++ script/footer.js
@@ -0,0 +1,9 @@
+const footer = ()=>{
+    let card = `<div>
+    <h4>Made by GAYATHRI <span>Copyright © 2024. All Rights Reserved.</span></h4>
+</div>`;
+
+   document.getElementById("footer").innerHTML = card
+};
+
+footer()
--- script/navbar.js
+++ script/navbar.js
@@ -1,7 +1,8 @@
 const navbar = ()=>{
      let card = ` <div id="nav-container">
         <div class="flex-element">
-            <div id="logo">My Personal App</div>
+            <a id="logo" href="index.html">My Personal App</a>
+
             <div id="nav-links">
                 <a href="signup.html">Signup</a>
                 <a href="login.html">Login</a>
