66115d36cae36fbf0c9394fd126b7ce295345562
ragayathri97 <ragayathri97@gmail.com>
2024-12-24 23:40:15 +0530

    commit3

--- script/signup.js
+++ script/signup.js
@@ -6,10 +6,33 @@ form.addEventListener("submit", function () {
   let password = form.password.value;
   let gender = form.gender.value;
   let mobile = form.mobile.value;
-
-
-//logic is check whether email is present in the DB
-fetch("http://localhost:3000/users+")
-
-  alert("signup sucessfull")
-})
+  let userObj = { username, email, password, gender, mobile };
+  /// logic is check whether email is present in the DB
+  fetch(`${baseUrl}/users`)
+    .then((res) => res.json())
+    .then((data) => {
+      let user = data.filter((el, i) => el.email == email);
+      if (user.length != 0) {
+        /// user present
+        alert("User already registred, please login");
+        window.location.href = "login.html"
+      } else {
+        /// user is not present
+        /// push the user into json server
+        fetch(`${baseUrl}/users`, {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(userObj),
+        }).then(() => {
+          alert("Signup Sucessfull");
+          window.location.href = "login.html"
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("Something wenr wrong, Please try again later");
+    });
+});
