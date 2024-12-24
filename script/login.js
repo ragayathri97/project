c5a8839b24339dd23286f3d4918ade0c049d2dd5
ragayathri97 <ragayathri97@gmail.com>
2024-12-25 00:12:08 +0530

    commit3

--- script/login.js
+++ script/login.js
@@ -4,11 +4,8 @@ import { baseUrl } from "./baseUrl.js";
 let form = document.getElementById("form");
 form.addEventListener("submit", function () {
   event.preventDefault();
-  let username = form.username.value;
   let email = form.email.value;
   let password = form.password.value;
-  let gender = form.gender.value;
-  let mobile = form.mobile.value;
   let userObj = { username, email, password, gender, mobile };
   
   /// logic is check whether email is present in the DB
@@ -18,21 +15,19 @@ form.addEventListener("submit", function () {
       let user = data.filter((el, i) => el.email == email);
       if (user.length != 0) {
         /// user present
-        alert("User already registred, please login");
-        window.location.href = "login.html"
+        //check for password
+       if(user[0].password==password){
+        alert("Login Sucess....")
+        window.location.href = "todos.html"
+       }
+       else{
+        alert("password is wrong, login with write password")
+       }
+       
       } else {
-        /// user is not present
-        /// push the user into json server
-        fetch(`${baseUrl}/users`, {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify(userObj),
-        }).then(() => {
-          alert("Signup Sucessfull");
-          window.location.href = "login.html"
-        });
+        alert("User not registred, please signup...")
+        window.location.href = "signup.html"
+
       }
     })
     .catch((err) => {
